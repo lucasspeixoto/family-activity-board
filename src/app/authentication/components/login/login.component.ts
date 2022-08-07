87d58730ffe6e57153463750de3c1888d889aeee
Lucas Peixoto <lspeixotodev@gmail.com>
2022-08-07 17:57:47 -0300

    Fix prettier endOfLine bug and include appButton css

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -9,6 +9,7 @@ export class LoginComponent implements OnInit {
   public defaultElevation = 2;
   public raisedElevation = 8;
   public readonly year = new Date().getFullYear();
+
   constructor() {}
 
   ngOnInit() {
