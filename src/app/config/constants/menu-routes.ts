d49947c3f1c39eb0dbcab8583c39c663204a9042
Lucas Peixoto <lspeixotodev@gmail.com>
2022-11-05 19:29:06 -0300

    Fix profile comming soon page

--- src/app/config/constants/menu-routes.ts
+++ src/app/config/constants/menu-routes.ts
@@ -4,11 +4,15 @@ import { MenuItem } from '@sharedMd/menu-item.model';
 export const menuItems: MenuItem[] = [
   { routerLink: appRoutes.BILLS, icon: 'event', label: 'Contas' },
   {
+    routerLink: appRoutes.TASKS,
+    icon: 'insert_chart_outlined',
+    label: 'Tarefas',
+  },
+  {
     routerLink: appRoutes.PLANS,
     icon: 'description',
     label: 'Planejamentos',
   },
-  { routerLink: appRoutes.TASKS, icon: 'assignment', label: 'Tarefas' },
   {
     routerLink: appRoutes.REPORTS,
     icon: 'dashboard',
--- src/app/features/bills/pipes/type-filter/type-filter.pipe.ts
+++ src/app/features/bills/pipes/type-filter/type-filter.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 const typeOptions: Record<number, string> = {
-  1: 'Contas (Ex: Energia, água, etc...',
+  1: 'Contas (Ex: Energia, água, etc...)',
   2: 'Cartão de Crédito',
   3: 'Faculdade',
   4: 'Cartão Supermercado',
