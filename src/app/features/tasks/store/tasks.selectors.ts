4ade1bf68f925faa4e77ccb2c12821083149cac2
Lucas Peixoto <lspeixotodev@gmail.com>
2022-11-08 06:20:03 -0300

    Include tasks notifications

--- src/app/features/tasks/store/tasks.selectors.ts
+++ src/app/features/tasks/store/tasks.selectors.ts
@@ -1,7 +1,14 @@
 import * as fromTasks from './tasks.reducer';
 
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { getTasksNotifications } from '../helpers/tasks-notifications.helper';
 
 const getTasksState = createFeatureSelector<fromTasks.TasksState>('tasks');
 
 export const getTasks = createSelector(getTasksState, state => state.tasks);
+
+export const getTasksNotification = createSelector(getTasks, tasks => {
+  const tasksNotifications = getTasksNotifications(tasks);
+
+  return tasksNotifications;
+});
--- src/app/shared/services/notifications/notifications.service.ts
+++ src/app/shared/services/notifications/notifications.service.ts
@@ -6,6 +6,7 @@ import { getBillsNotification } from '@billsSt/bills.selectors';
 import { Injectable } from '@angular/core';
 import { Notification } from '@sharedMd/notification';
 import { Store } from '@ngrx/store';
+import { getTasksNotification } from '@app/features/tasks/store/tasks.selectors';
 
 @Injectable({
   providedIn: 'root',
@@ -19,13 +20,17 @@ export class NotificationsService {
     return this._store.select(getBillsNotification);
   }
 
+  public getTasksNotifications(): Observable<Notification> {
+    return this._store.select(getTasksNotification);
+  }
+
   public getNotificationsAmount(): Observable<number> {
     const notificationsAmount$ = combineLatest([
       this.getBillsNotifications(),
-      of(0), //Simulação de Próxima feature para quadro de notificações
+      this.getTasksNotifications(),
     ]).pipe(
-      map(([billsAmount, plansAmount]) => {
-        return billsAmount.amount + plansAmount;
+      map(([billsAmount, tasksAmount]) => {
+        return billsAmount.amount + tasksAmount.amount;
       })
     );
 
